b55e9f76f512d8f282480f909745712604652e30
0xw01f <76235339+0xw01f@users.noreply.github.com>
2021-03-15 02:37:27 +0100

    Added welcome email sending

---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,9 @@
+@component('mail::message')
+# Welcome to w01fgram
+
+This is a community of non-sense people, and we are so happy that you joined us !
+
+
+All the best,<br>
+{{ config('app.name') }}
+@endcomponent
